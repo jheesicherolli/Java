44b782d25d6f1d99c2a17e008f04e33d11982018
jheesicherolli <jennifer.sicherolli@gmail.com>
2022-05-10 23:59:00 -0300

    Mudando arquivos

--- ExerciciosJava/src/ProgramacaoSequencial/Atividade5.java
+++ ExerciciosJava/src/ProgramacaoSequencial/Atividade5.java
@@ -1,4 +1,4 @@
-package ProgramacaoSequencialJava;
+package ProgramacaoSequencial;
 
 import java.util.Scanner;
 
--- ExerciciosJava/src/ProgramacaoSequencial/HelloWorld.java
+++ ExerciciosJava/src/ProgramacaoSequencial/HelloWorld.java
@@ -1,4 +1,4 @@
-package ProgramacaoSequencialJava;
+package ProgramacaoSequencial;
 
 public class HelloWorld {
 
